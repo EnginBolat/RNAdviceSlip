c2318f1b94d47391b74b8bfbad940b4aa9177dfa
Engin Bolat <engi.bolat@gmail.com>
2024-05-04 01:19:11 +0300

    first commit

---
+++ app/pages/home/_layout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { View,Text } from "react-native";
+
+export default function Home() {
+    return <View className="flex flex-1 justify-center items-center">
+        <Text>Hello from home page</Text>
+    </View>
+}
